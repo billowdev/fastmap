d4055bafa3a5cbfc57a2060b4658cc558d1f6b93
Akkarapon Phikulsri <akkarapon@looksocial.asia>
2025-01-26 16:29:41 +0700

    test: Update function_benchmark_test.go

--- hashmap/functional_benchmark_test.go
+++ hashmap/functional_benchmark_test.go
@@ -7,42 +7,58 @@ import (
 	fastmap "github.com/billowdev/fastmap/hashmap"
 )
 
-func BenchmarkHashMapFilter(b *testing.B) {
+func BenchmarkHashMapFilterLarge(b *testing.B) {
 	h := fastmap.NewHashMap[string, int]()
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 10000; i++ {
 		h.Put(fmt.Sprintf("key%d", i), i)
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		h.Filter(func(k string, v int) bool {
-			return v%2 == 0
+			return v%2 == 0 && v%3 == 0
 		})
 	}
 }
 
-func BenchmarkHashMapMap(b *testing.B) {
+func BenchmarkHashMapMapLarge(b *testing.B) {
 	h := fastmap.NewHashMap[string, int]()
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 10000; i++ {
 		h.Put(fmt.Sprintf("key%d", i), i)
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		h.Map(func(k string, v int) int {
-			return v * 2
+			return v*2 + v%3
 		})
 	}
 }
 
-func BenchmarkHashMapForEach(b *testing.B) {
+func BenchmarkHashMapForEachLarge(b *testing.B) {
 	h := fastmap.NewHashMap[string, int]()
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 10000; i++ {
 		h.Put(fmt.Sprintf("key%d", i), i)
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		sum := 0
 		h.ForEach(func(k string, v int) {
-			sum += v
+			sum += v % 10
+		})
+	}
+}
+
+func BenchmarkHashMapChainedOperations(b *testing.B) {
+	h := fastmap.NewHashMap[string, int]()
+	for i := 0; i < 1000; i++ {
+		h.Put(fmt.Sprintf("key%d", i), i)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		filtered := h.Filter(func(k string, v int) bool {
+			return v%2 == 0
+		})
+		filtered.Map(func(k string, v int) int {
+			return v * 2
 		})
 	}
 }
