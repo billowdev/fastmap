70e2558c5ac96f9dc10b06cbc13b841489d1d8c7
Akkarapon Phikulsri <akkarapon@looksocial.asia>
2025-01-26 16:39:01 +0700

    test: coverage

--- sample/main.go
+++ sample/main.go
@@ -1,7 +1,9 @@
 package main
+
 import (
-	fastmap "github.com/billowdev/fastmap/core"
+	fastmap "github.com/billowdev/fastmap/hashmap"
 )
+
 func main() {
 	// Example usage
 	hashMap := fastmap.NewHashMap[string, int]()
